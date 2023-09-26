01e7125efd3e16515ac439c5ff28b577e56bb6cc
Letícia Varela <131541653+Leticiavarela@users.noreply.github.com>
2023-09-26 20:38:05 -0300

    Create exercicio07.py

---
+++ Python/exercicio07.py
@@ -0,0 +1,5 @@
+n1 = float (input('Primeira nota: '))
+n2 = float (input('Segunda nota: '))
+r = (n1 + n2)/2
+print('Sua média é: {}'.format(r))
+ ### Sempre se atentar a ordem de precedencia dos operadores para obter o resultado correto.
