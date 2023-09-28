136bbc45e8262efd0c45cc41cf554f0c07eece6c
Letícia Varela <131541653+Leticiavarela@users.noreply.github.com>
2023-09-28 18:45:07 -0300

    Create exercicio09.py

---
+++ exercicio09.py
@@ -0,0 +1,13 @@
+n = int (input('Digite um número: '))
+r1 = (n)*1
+r2 = (n)*2
+r3 = (n)*3
+r4 = (n)*4
+r5 = (n)*5
+r6 = (n)*6
+r7 = (n)*7
+r8 = (n)*8
+r9 = (n)*9
+r10 = (n)*10
+print('\nTabuada {}\n \n6 x 1 = {}\n6 x 2 = {}\n6 x 3 = {}\n6 x 4 = {}\n6 x 5 = {}'.format(n , r1, r2, r3, r4, r5,))
+print('6 x 6 = {}\n6 x 7 = {}\n6 x 8 = {}\n6 x 9 = {}\n6 x 10 = {}' .format(r6, r7, r8, r9, r10 ))
